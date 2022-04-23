8ce5ee2f993720ff78365c417278fac8a9a37f91
34695 <wvillaor7@alumnes.ub.edu>
2022-04-24 01:48:00 +0200

    registro

--- app/src/main/java/com/example/projectepis/Registro.java
+++ app/src/main/java/com/example/projectepis/Registro.java
@@ -1,21 +1,48 @@
 package com.example.projectepis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registro extends AppCompatActivity {
     EditText etDate;
+    EditText etName;
+    EditText etApellido;
+    EditText etEmail;
+    EditText etPassword;
+    Button btRegistro;
+
+    //Variables de datos que vamos a registrar
+    String name="";
+    String apellido="";
+    String email="";
+    String password="";
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
     DatePickerDialog.OnDateSetListener setListener;
     TextInputLayout genero;
     AutoCompleteTextView autoCompleteTextView;
@@ -27,6 +54,14 @@ public class Registro extends AppCompatActivity {
         autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
         etDate = findViewById(R.id.et_date);
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        etName = (EditText) findViewById(R.id.et_Nombre);
+        etApellido = (EditText) findViewById(R.id.et_Apellido);
+        etEmail = (EditText) findViewById(R.id.et_Email);
+        etPassword = (EditText) findViewById(R.id.et_Password);
+        btRegistro = (Button) findViewById(R.id.bt_Registrar);
 
         //Crear array de genero
         String [] generos = new String[]{
@@ -66,8 +101,73 @@ public class Registro extends AppCompatActivity {
             }
         });
 
+        btRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = etName.getText().toString();
+                apellido = etApellido.getText().toString();
+                email = etEmail.getText().toString();
+                password = etPassword.getText().toString();
+
+                if(!name.isEmpty() && !apellido.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+
+                   if(password.length()>=6){
+
+                       registerUser();
+
+                   }else{
+                       Toast.makeText(Registro.this, "El password debe tener al menos 6 caracteres", Toast.LENGTH_SHORT ).show();
+                   }
+
+                }else{
+                    Toast.makeText(Registro.this, "Debes completar los campos", Toast.LENGTH_SHORT ).show();
+                }
+
+
+            }
+        });
 
 
 
     }
+
+    private void registerUser(){
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    Map<String , Object> map = new HashMap<>();
+                    map.put("nombre",name );
+                    map.put("apellido",apellido );
+                    map.put("email",email );
+                    map.put("password",password );
+
+                    String id = mAuth.getCurrentUser().getUid();
+
+
+                    mDatabase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if(task2.isSuccessful()){
+
+                                startActivity(new Intent(Registro.this,MainActivity.class));
+                                finish();
+                            }else{
+                                Toast.makeText(Registro.this, "No se pudo crear los datos correctamente", Toast.LENGTH_SHORT ).show();
+                            }
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(Registro.this, "No se pudo registrar este usuario", Toast.LENGTH_SHORT ).show();
+                }
+            }
+        });
+
+    }
+
+
+
 }
